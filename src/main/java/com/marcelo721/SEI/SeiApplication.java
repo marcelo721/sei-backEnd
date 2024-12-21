dbd176355ee31b074df8a3f59e01cbba0bb476c4
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-12-21 14:09:47 -0300

    first commit

---
+++ src/main/java/com/marcelo721/SEI/SeiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo721.SEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo721/SEI/SeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo721.SEI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
