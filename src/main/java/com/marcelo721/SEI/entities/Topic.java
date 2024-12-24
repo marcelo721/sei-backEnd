29823a1b3a6ac30b96f92910ad1939a11f0210b4
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-12-24 10:42:19 -0300

    feat: create Topic entity

---
+++ src/main/java/com/marcelo721/SEI/entities/Topic.java
@@ -0,0 +1,24 @@
+package com.marcelo721.SEI.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false, name = "id")
+    private Long id;
+
+    @Column(nullable = false,length = 50, unique = true, name = "name")
+    private String name;
+
+    @Column(nullable = false, length = 100, name = "description")
+    private String description;
+}
