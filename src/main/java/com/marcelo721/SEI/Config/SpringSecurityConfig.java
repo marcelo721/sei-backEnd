52f76faed70c5bafebd3db046ef312cb7c6059e0
marcelo721 <marceloh.sousa@alu.ufc.br>
2025-02-28 09:19:53 -0300

    refactor(webconfig and SpringSecurityConfig): added new bean to Cors

--- src/main/java/com/marcelo721/SEI/Config/SpringSecurityConfig.java
+++ src/main/java/com/marcelo721/SEI/Config/SpringSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,22 +38,25 @@ public class SpringSecurityConfig {
     };
 
     @Bean
-    public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults()) // Habilita o CORS com configurações padrão
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth ->
-                        auth.
-                                requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll().
-                                requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll().
-                                requestMatchers(HttpMethod.GET, "/api/v1/users/verify").permitAll().
-                                requestMatchers(HttpMethod.POST, "/api/v1/forgot-password/**").permitAll().
-                                requestMatchers(DOCUMENTATION_OPENAPI).permitAll().anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/verify").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/forgot-password/**").permitAll()
+                        .requestMatchers(DOCUMENTATION_OPENAPI).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtSecurityFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
-                .exceptionHandling(ex -> ex.authenticationEntryPoint(new CustomAuthenticationEntryPoint())).build();
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+                .build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/marcelo721/SEI/Config/WebConfig.java
+++ src/main/java/com/marcelo721/SEI/Config/WebConfig.java
@@ -1,19 +1,28 @@
 package com.marcelo721.SEI.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000") // Front-end URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
+
 
